6240b5e6c50c64d7773b83f74e864935b3a9b9a0
Bitumnus <dariia.zamrachilo@gmail.com>
2020-05-09 13:50:51 +0300

    cleare code

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,42 +4,6 @@ import axios from '../axios/api';
 import './index.css';
 import Loader from '../UI/Loader/Loader';
 
-/*
-
-avatar_url: "https://avatars3.githubusercontent.com/u/15800478?v=4"
-bio: null
-blog: ""
-company: "@Novell"
-created_at: "2015-11-11T13:13:57Z"
-email: null
-events_url: "https://api.github.com/users/bitumnus/events{/privacy}"
-followers: 0
-followers_url: "https://api.github.com/users/bitumnus/followers"
-following: 0
-following_url: "https://api.github.com/users/bitumnus/following{/other_user}"
-gists_url: "https://api.github.com/users/bitumnus/gists{/gist_id}"
-gravatar_id: ""
-hireable: null
-html_url: "https://github.com/bitumnus"
-id: 15800478
-location: "Kiev"
-login: "bitumnus"
-name: "Dariia"
-node_id: "MDQ6VXNlcjE1ODAwNDc4"
-organizations_url: "https://api.github.com/users/bitumnus/orgs"
-public_gists: 0
-public_repos: 7
-received_events_url: "https://api.github.com/users/bitumnus/received_events"
-repos_url: "https://api.github.com/users/bitumnus/repos"
-site_admin: false
-starred_url: "https://api.github.com/users/bitumnus/starred{/owner}{/repo}"
-subscriptions_url: "https://api.github.com/users/bitumnus/subscriptions"
-type: "User"
-updated_at: "2020-04-27T10:52:55Z"
-url: "https://api.github.com/users/bitumnus"
-
-*/
-
 function Profile(props) {
 
     const [login, setLogin] = useState('')
