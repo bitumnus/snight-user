69d32d10bd16b44e6c9dd5b6195032d067373359
Bitumnus <dariia.zamrachilo@gmail.com>
2020-05-16 21:12:30 +0300

    Updates

---
+++ precache-manifest.026ebd43171d206bf871b73735257453.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1239cef17656a5429afa0405b198ead5",
+    "url": "/snight-user/index.html"
+  },
+  {
+    "revision": "f99f01918981c9d7c3e7",
+    "url": "/snight-user/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "34ff17ae8d0ec477c302",
+    "url": "/snight-user/static/css/main.f4a663c6.chunk.css"
+  },
+  {
+    "revision": "f99f01918981c9d7c3e7",
+    "url": "/snight-user/static/js/2.119b162b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/snight-user/static/js/2.119b162b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ff17ae8d0ec477c302",
+    "url": "/snight-user/static/js/main.407fce0b.chunk.js"
+  },
+  {
+    "revision": "c326e49e5fc495c5e9f5",
+    "url": "/snight-user/static/js/runtime-main.ec73c3e7.js"
+  }
+]);
